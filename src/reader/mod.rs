7b329f358c6815d2148804e5db1aae65f6812abc
Jack Lloyd <jack.lloyd@fortanix.com>
2019-02-21 11:39:23 -0500

    Fixes for older Rust

--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -25,6 +25,8 @@ use super::models::{ObjectIdentifier,TaggedDerValue};
 use super::models::{UTCTime,GeneralizedTime};
 pub use self::error::*;
 
+use std::ascii::AsciiExt;
+
 /// Parses DER/BER-encoded data.
 ///
 /// [`parse_ber`][parse_ber] and [`parse_der`][parse_der] are shorthands
